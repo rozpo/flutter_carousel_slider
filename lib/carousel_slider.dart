493b56b6d1de4d247a40f2fdb8145b86ce1f042d
Piotr Rozpończyk <rozpo.dev@gmail.com>
2024-08-08 12:49:13 +0100

    Package fix after conflict with Flutter 3.24.0

--- lib/carousel_slider.dart
+++ lib/carousel_slider.dart
@@ -4,7 +4,7 @@ import 'dart:async';
 
 import 'package:carousel_slider/carousel_state.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide CarouselController;
 
 import 'carousel_controller.dart';
 import 'carousel_options.dart';
@@ -355,7 +355,7 @@ class CarouselSliderState extends State<CarouselSlider>
                 BuildContext storageContext = carouselState!
                     .pageController!.position.context.storageContext;
                 final double? previousSavedPosition =
-                    PageStorage.of(storageContext)?.readState(storageContext)
+                    PageStorage.of(storageContext).readState(storageContext)
                         as double?;
                 if (previousSavedPosition != null) {
                   itemOffset = previousSavedPosition - idx.toDouble();
